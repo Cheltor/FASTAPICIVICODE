32631ceda45a720610cf9f679b541af1059648c3
Cheltor <rchelton@umd.edu>
2024-10-16 16:53:37 -0400

    Refactor database.py to load environment variables from .env file

--- CiviCodeAPI/database.py
+++ CiviCodeAPI/database.py
@@ -1,9 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+# Load the environment variables
+load_dotenv()
 
 # Replace this with your actual database URL
-DATABASE_URL = "postgresql://rchelton:password@localhost:5433/codeenforcement_development"
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://rchelton:password@localhost:5433/codeenforcement_development")
 
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
