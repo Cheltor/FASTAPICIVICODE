68f592a99f3eefa6df65a9fba5d1d2ec848f6192
Cheltor <rchelton@umd.edu>
2025-09-22 20:48:58 -0400

    Fix import path for models in database.py and add __init__.py to CiviCodeAPI package

--- CiviCodeAPI/database.py
+++ CiviCodeAPI/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-from .models import Base
+from models import Base
 
 # Load environment variables from .env for local dev
 load_dotenv()
